a2beefa5f60c5db85e631c0b10a2df786bee651c
HemaGuvvala <guvvala.hema@gmail.com>
2025-03-11 02:21:43 -0700

    Created a Server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+
+// request handler function
+app.use("/test",(req,res) => {
+  res.send("Hello from the server!");
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is successfully listening on port 3000");
+});
