9d79e598356129f7ef50300167f2689b8b8316ea
HemaGuvvala <guvvala.hema@gmail.com>
2025-03-13 02:53:46 -0700

    Writing auth middleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
 const express = require("express");
 
 const app = express();
+const {userAuth, adminAuth} = require("./middlewares/auth");
+// Handle Auth Middleware for all GET, PUT,.... requests
+app.use("/admin", adminAuth);
 
-app.use("/user",(req,res,next)=>{
-  // Route Handler
-  console.log("Handling the route user")
-  //res.send("Route Handler")
-  next();
-  
-},(req,res,next)=>{
-  //route handler 2
-  console.log("Handling the route user2")
- // res.send("Route Handler2")
-  next();
-},(req,res,next)=>{
-  console.log("Handling the route user3")
-  res.send("Route Handler3")
-  //next()
+app.get("/user",userAuth,(req,res)=>{
+  // Check if the request is authorized
+    res.send("user Data Sent") 
+  })
+app.get("/admin/getAllData",(req,res)=>{
+// Check if the request is authorized
+  res.send("All Data Sent") 
+})
+
+app.get("/admin/deleteUser",(req,res)=>{
+  res.send("Delete a User")
 })
 
 app.listen(3000, ()=>{
